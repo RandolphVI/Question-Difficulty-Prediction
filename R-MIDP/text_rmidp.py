4962b51b2266b4e32a0b2d99bcc9741bcc06a458
RandolphVI <chinawolfman@hotmail.com>
2019-11-28 16:51:27 +0800

    R-MIDP model added.

---
+++ R-MIDP/text_rmidp.py
@@ -0,0 +1,115 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Randolph'
+
+import numpy as np
+import tensorflow as tf
+
+
+class TextRMIDP(object):
+    """A RMIDP for text classification."""
+
+    def __init__(
+            self, sequence_length, vocab_size, fc_hidden_size, lstm_hidden_size, embedding_size,
+            embedding_type, l2_reg_lambda=0.0, pretrained_embedding=None):
+
+        # Placeholders for input, output, dropout_prob and training_tag
+        self.input_x_content = tf.placeholder(tf.int32, [None, sequence_length[0]], name="input_x_content")
+        self.input_x_question = tf.placeholder(tf.int32, [None, sequence_length[1]], name="input_x_question")
+        self.input_x_option = tf.placeholder(tf.int32, [None, sequence_length[2]], name="input_x_option")
+        self.input_y = tf.placeholder(tf.float32, [None, 1], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+        self.is_training = tf.placeholder(tf.bool, name="is_training")
+
+        self.global_step = tf.Variable(0, trainable=False, name="Global_Step")
+
+        def _fc_layer(input_x, name=""):
+            """
+            Fully Connected Layer.
+            Args:
+                input_x:
+                name: Scope name
+            Returns:
+                [batch_size, fc_hidden_size]
+            """
+            with tf.name_scope(name + "fc"):
+                num_units = input_x.get_shape().as_list()[-1]
+                W = tf.Variable(tf.truncated_normal(shape=[num_units, fc_hidden_size],
+                                                    stddev=0.1, dtype=tf.float32), name="W")
+                b = tf.Variable(tf.constant(value=0.1, shape=[fc_hidden_size], dtype=tf.float32), name="b")
+                fc = tf.nn.xw_plus_b(input_x, W, b)
+                fc_out = tf.nn.relu(fc)
+            return fc_out
+
+        def _bi_lstm(input_x, name=""):
+            # Bi-LSTM Layer
+            with tf.variable_scope(name + "Bi_lstm", reuse=tf.AUTO_REUSE):
+                lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(lstm_hidden_size)  # forward direction cell
+                lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(lstm_hidden_size)  # backward direction cell
+                if self.dropout_keep_prob is not None:
+                    lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=self.dropout_keep_prob)
+                    lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=self.dropout_keep_prob)
+
+                # Creates a dynamic bidirectional recurrent neural network
+                # shape of `outputs`: tuple -> (outputs_fw, outputs_bw)
+                # shape of `outputs_fw`: [batch_size, sequence_length, lstm_hidden_size]
+
+                # shape of `state`: tuple -> (outputs_state_fw, output_state_bw)
+                # shape of `outputs_state_fw`: tuple -> (c, h) c: memory cell; h: hidden state
+                outputs, state = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, input_x, dtype=tf.float32)
+
+            # Concat output
+            # [batch_size, sequence_length, lstm_hidden_size * 2]
+            lstm_out = tf.concat(outputs, axis=2, name=name + "lstm_out")
+
+            # [batch_size, lstm_hidden_size * 2]
+            lstm_pooled = tf.reduce_max(lstm_out, axis=1, name=name + "lstm_pooled")
+
+            return lstm_pooled
+
+        # Embedding Layer
+        with tf.device("/cpu:0"), tf.name_scope("embedding"):
+            # Use random generated the word vector by default
+            # Can also be obtained through our own word vectors trained by our corpus
+            if pretrained_embedding is None:
+                self.embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], minval=-1.0, maxval=1.0,
+                                                               dtype=tf.float32), trainable=True, name="embedding")
+            else:
+                if embedding_type == 0:
+                    self.embedding = tf.constant(pretrained_embedding, dtype=tf.float32, name="embedding")
+                if embedding_type == 1:
+                    self.embedding = tf.Variable(pretrained_embedding, trainable=True,
+                                                 dtype=tf.float32, name="embedding")
+            # [batch_size, sequence_length, embedding_size]
+            self.embedded_sentence_content = tf.nn.embedding_lookup(self.embedding, self.input_x_content)
+            self.embedded_sentence_question = tf.nn.embedding_lookup(self.embedding, self.input_x_question)
+            self.embedded_sentence_option = tf.nn.embedding_lookup(self.embedding, self.input_x_option)
+
+        # Concat -> embedded_sentence_all: [batch_size, sequence_length_all, embedding_size]
+        self.embedded_sentence_all = tf.concat([self.embedded_sentence_content, self.embedded_sentence_question,
+                                               self.embedded_sentence_option], axis=1)
+
+        # Bi-LSTM Layer
+        # bi_lstm_out: [batch_size, lstm_hidden_size * 2]
+        self.bi_lstm_out = _bi_lstm(self.embedded_sentence_all, name="total_")
+
+        # Fully Connected Layer
+        self.fc_out = _fc_layer(self.bi_lstm_out)
+
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.fc_drop = tf.nn.dropout(self.fc_out, self.dropout_keep_prob)
+
+        # Final scores
+        with tf.name_scope("output"):
+            W = tf.Variable(tf.truncated_normal(shape=[fc_hidden_size, 1],
+                                                stddev=0.1, dtype=tf.float32), name="W")
+            b = tf.Variable(tf.constant(value=0.1, shape=[1], dtype=tf.float32), name="b")
+            self.logits = tf.nn.xw_plus_b(self.fc_drop, W, b, name="logits")
+            self.scores = tf.sigmoid(self.logits, name="scores")
+
+        # Calculate mean cross-entropy loss, L2 loss
+        with tf.name_scope("loss"):
+            losses = tf.reduce_mean(tf.square(self.input_y - self.scores), name="losses")
+            l2_losses = tf.add_n([tf.nn.l2_loss(tf.cast(v, tf.float32)) for v in tf.trainable_variables()],
+                                 name="l2_losses") * l2_reg_lambda
+            self.loss = tf.add(losses, l2_losses, name="loss")
